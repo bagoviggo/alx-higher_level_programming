647012426b21ff3f9d66909e0dfb47a01a350782
bagoviggo <bagoviggo@gmail.com>
2022-12-04 07:45:31 +0000

    change or to and

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     list_len = len(my_list)
-    if idx > list_len or idx < 0:
+    if idx > list_len and idx < 0:
         return None
     else:
         return my_list[idx]
