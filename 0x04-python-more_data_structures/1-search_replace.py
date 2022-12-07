1c6f328d32dca29d79c4302449ed5a54bfabf8e1
bagoviggo <bagoviggo@pop-os.localdomain>
2022-12-07 17:29:59 +0300

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that replaces all occurrences of an element by another in a new list."""
+
+
+def search_replace(my_list, search, replace):
+    if search in my_list:
+        return [replace if x == search else x for x in my_list]
