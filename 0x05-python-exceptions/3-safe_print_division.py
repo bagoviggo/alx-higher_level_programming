a0cce390f3628572b0e4ec736d85cb9f5a79a214
bagoviggo <bagoviggo@pop-os.localdomain>
2022-12-19 20:05:08 +0300

    3-safe_print_division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result."""
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
