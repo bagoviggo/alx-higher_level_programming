69602c0c5b2e6e984444b98bd48d9596851cda7d
bagoviggo <bagoviggo@pop-os.localdomain>
2022-12-20 21:48:20 +0300

    heavily commented 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,14 @@
 
 
 class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
         self.size = size
