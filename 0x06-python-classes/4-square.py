37fda405de202bd797207e6e701568ce71efd205
bagoviggo <bagoviggo@pop-os.localdomain>
2022-12-20 22:23:55 +0300

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+
+"""Square class definition"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of a square
+        Returns: The area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """getter for __size"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """setter for __size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
