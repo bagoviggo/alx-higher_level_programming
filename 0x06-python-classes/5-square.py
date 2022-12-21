b7b65089fc3db699439bc50889c894ea69add72a
bagoviggo <bagoviggo@pop-os.localdomain>
2022-12-21 13:33:17 +0300

    fix all

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -8,7 +8,7 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initializes a square
         Args:
             size (int): size of a side of the square
