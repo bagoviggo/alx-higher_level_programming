1fea0ebcf491946012b74b708bb2bf8491213eb7
bagoviggo <bagoviggo@pop-os.localdomain>
2022-12-21 13:43:00 +0300

    complete fix 4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -25,7 +25,7 @@ class Square:
     def size(self):
         """getter for __size"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """setter for __size"""
