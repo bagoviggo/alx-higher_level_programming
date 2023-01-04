ab4eefe56876918372592240e8e9fc6e0f14b539
bagoviggo <bagoviggo@pop-os.localdomain>
2023-01-04 22:11:11 +0300

    fix on 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" This module contains the function that defines a rectangle """
+""" This module contains an empty class that defines a rectangle """
 
 
 class Rectangle:
+    """ This is an empty class that defines a rectangle """
     pass
