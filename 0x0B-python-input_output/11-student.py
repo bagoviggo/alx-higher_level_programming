705e7e7e487145f342c812098d74b03de4ea664b
bagoviggo <bagoviggo@pop-os.localdomain>
2023-01-11 00:56:51 +0300

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A class Student that defines a student by: (based on 10-student.py)"""
+
+
+class Student:
+    """A class Student that defines a student by: (based on 10-student.py)"""
+
+    def __init__(self, first_name, last_name, age):
+        """A class Student that defines a student by: (based on 10-student.py)"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """A class Student that defines a student by: (based on 10-student.py)"""
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """A class Student that defines a student by: (based on 10-student.py)"""
+        for i, j in json.items():
+            setattr(self, i, j)
