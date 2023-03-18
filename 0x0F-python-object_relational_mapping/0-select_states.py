e3b7ecd819ccdfdc43c1a70947cd2722aa722768
bagoviggo <bagoviggo@gmail.com>
2023-03-18 18:18:46 +0300

    add 0-select_states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    # Check the number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get the arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306)
+
+    # Get a cursor
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
