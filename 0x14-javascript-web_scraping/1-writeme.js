e419947943346a79cb681a13d66712aec501517e
bagoviggo <bagoviggo@gmail.com>
2023-04-26 13:16:57 +0300

    fix task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,14 +3,12 @@
 // Script that writes to a file
 // Usage: ./1-writeme.js <path> <file>
 
-
 const fs = require('fs');
 const path = process.argv[2];
 const string = process.argv[3];
 
 fs.writeFile(path, string, 'utf-8', (error) => {
-    if (error) {
-        console.log(error);
-    }
+  if (error) {
+    console.log(error);
+  }
 });
-
