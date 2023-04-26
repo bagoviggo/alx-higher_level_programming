322a5d685d2acffb82ee6f19b30177e5ebf241f1
bagoviggo <bagoviggo@gmail.com>
2023-04-26 13:14:09 +0300

    Add Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// Script that writes to a file
+// Usage: ./1-writeme.js <path> <file>
+
+
+const fs = require('fs');
+const path = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(path, string, 'utf-8', (error) => {
+    if (error) {
+        console.log(error);
+    }
+});
+
