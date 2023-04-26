d2ac25da9c4b3af90d319b4d43c9cca4561e62a0
bagoviggo <bagoviggo@gmail.com>
2023-04-26 16:37:09 +0300

    semistd fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,9 +7,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('code:', response && response.statusCode);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });
