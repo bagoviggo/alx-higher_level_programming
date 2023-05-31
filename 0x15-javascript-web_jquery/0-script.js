b6000c1e29c8b0bec9fd15f010609061319cb256
bagoviggo <bagoviggo@gmail.com>
2023-06-01 01:51:01 +0300

    Add 0-script.js & 1-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Script that updates text color of <header>
+const headerElement = document.querySelector('header');
+
+//change color of header
+headerElement.style.color ='#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+// Script that updates the color of <header with jquery
+$(function updateHeader() {
+    $('header').css('color', '#FF0000');
+});
