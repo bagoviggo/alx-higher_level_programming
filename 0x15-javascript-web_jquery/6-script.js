c8e8cb4dd3008c48a6acc3e6135f3c2da73bd71b
bagoviggo <bagoviggo@gmail.com>
2023-06-01 04:39:30 +0300

    Add task 6

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+/* script that updates the text of the <header> element to New Header!!!
+when the user clicks on DIV#update_header */
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
